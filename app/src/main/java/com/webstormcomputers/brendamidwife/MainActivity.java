be60e77b24434b9ab1ea1894bbccfa613befbf8f
Aaron Mitcham <aaron@webstormcomputers.com>
2018-04-27 08:38:19 -0500

    Initial push

--- app/src/main/java/com/webstormcomputers/brendamidwife/MainActivity.java
+++ app/src/main/java/com/webstormcomputers/brendamidwife/MainActivity.java
@@ -1,7 +1,10 @@
 package com.webstormcomputers.brendamidwife;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void phoneOnClick(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "1-409-739-3904"));
+        startActivity(phoneIntent);
+    }
+
+
+    public void webSiteButtonOnClick(View view)
+    {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.easttexasmidwife.com"));
+        startActivity(browserIntent);
     }
 }
